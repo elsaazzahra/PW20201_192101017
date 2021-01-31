3e2e097ee4d1cdd16428cfedf862cc0326e547dd
elsaazzahra <elsaapriliaazzahra14@gmail.com>
2021-01-31 19:58:51 +0700

    UAS

--- GAGALUAS-192101017-Elsa_Aprilia_Azzahra/app/controllers/Login.php
+++
@@ -1,5 +0,0 @@
-<?php
-    session_start();
-    include_once '../models/User.php';
-    $usr = $_POST['username'];
-    $pwd = $
--- GAGALUAS-192101017-Elsa_Aprilia_Azzahra/app/models/User_model.php
+++
@@ -1,9 +0,0 @@
-<?php
-class User_model
-{
- private $nama = "Fajar Septian";
- public function getUser()
- {
- return $this->nama;
- }
-}
